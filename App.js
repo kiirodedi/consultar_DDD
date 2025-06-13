b4dd3db8635db9b487e974500d9c086aae224c99
kiirodedi <moraismoralesdeborafernanda3@gmail.com>
2025-06-13 16:11:59 -0300

    Criação de função para coletar dados da API (13/06)

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as obj_DDD from './services/ddd.js';
 
 export default function App() {
+  obj_DDD.buscarDDDCallBack(11,retorno=>{
+    console.log(retorno)
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
